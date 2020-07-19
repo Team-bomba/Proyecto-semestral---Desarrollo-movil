3f6a359ff9bf0e300ec59ab6c6be62f6c9c83d32
Akatsuki507 <alfonso.alvarado.v31@gmail.com>
2020-07-19 12:34:29 -0500

    modelos basicos para uso del front

---
+++ app/src/main/java/com/example/proyectosemestral/models/Comment.java
@@ -0,0 +1,9 @@
+package com.example.proyectosemestral.models;
+
+public class Comment {
+    private int id;
+    private String body;
+    private int user_id;
+    private String commentable_type;
+    private int commentable_id;
+}
---
+++ app/src/main/java/com/example/proyectosemestral/models/User.java
@@ -0,0 +1,9 @@
+package com.example.proyectosemestral.models;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+
+}
